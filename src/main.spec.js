0565599a0dd8d21c4d029cf2de9e9e5ff01dc546
JohnLDev <john.lenon.dev@gmail.com>
2023-02-03 15:21:03 -0300

    test: making test not pass

--- src/main.spec.js
+++ src/main.spec.js
@@ -2,6 +2,6 @@ const { sum } = require('./main');
 
 describe('Test sum function', () => {
   it('should return 5', () => {
-    expect(sum(2, 3)).toBe(5);
+    expect(sum(2, 3)).toBe(6);
   });
 });
